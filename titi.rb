5abefc2478c33001beb4c1716c769dec94bc9c9b
laetitia69 <titi-fernandes@hotmail.fr>
2018-07-11 13:57:59 +0200

    tweet envoyé

---
+++ titi.rb
@@ -0,0 +1,13 @@
+require 'twitter'
+require 'dotenv/load'
+
+# quelques lignes qui enregistrent les clés d'APIs
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["TWITTER_API_KEY"]
+  config.consumer_secret     = ENV["TWITTER_API_SECRET"]
+  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+  config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
+end
+
+# ligne qui permet de tweeter
+client.update('Mon premier tweet en ruby !!!!')
